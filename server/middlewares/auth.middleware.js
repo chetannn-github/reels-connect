7437b6075a536ff1a456e0ad7295f2d79744d109
chetannn-github <chetan.rajawat25@gmail.com>
2025-09-18 18:46:49 +0530

    fix

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -15,7 +15,7 @@ export const protect = async (req, res, next) => {
         
 
 
-        const user = await User.findById("68cbf6a841738556edeadce5").select('-passwordHash');
+        const user = await User.findById("68cbf6d4ba59a1ee94320e56").select('-passwordHash');
 
         if (!user) {
             return res.status(401).json({ message: 'User not found' });
@@ -28,3 +28,6 @@ export const protect = async (req, res, next) => {
         res.status(401).json({ message: 'Not authorized' });
     }
 };
+
+
+client_id=1116520140680050&client_secret=51254aab5e1b99bb03748d688b2b5fee&grant_type=authorization_code&redirect_uri=https%3A%2F%2Freels-connect.onrender.com%2Fapi%2Fig%2Fcallback&code=AQD7W2ePXlwQiTmFSGvrMPI-2mUqCR2Bw8ERaVSkOj6obkd4PcSv1OWluHFgU7bg_WlgMSqHQded8QL3as4mHajpp759ULUiTTQIVL8toTHzFs0F8tQhwsQIEA6-73QbkzKy_b862g2yqy4aKKj-qGIaVtsvQDQIijTt05HWbd14pVpAbJYPrsqGusB5uNY2RWEcurKNDjjS-QhAVNJQJcX3X2jcCgyWPljOHreq6_stdA
