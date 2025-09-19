beb7c064abb2fd707862d53fa220ce51e5a059e8
chetannn-github <chetan.rajawat25@gmail.com>
2025-09-19 18:02:25 +0530

    fix - made reels separate collection

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -5,24 +5,26 @@ const reelSchema = new mongoose.Schema({
     reelTitle: { type: String, required: true },
     isActive: { type: Boolean, default: true },
     keywords: [{ type: String }],
-    message: { type: String , default : ""},
-    mediaURL : {type : String, default : ""},
-    timestamp : {type : String, default : ""}
-});
+    message: { type: String , default : "" },
+    mediaURL : { type : String, default : "" },
+    timestamp : { type : String, default : "" },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
+}, { timestamps: true });
 
+const Reel = mongoose.model('Reel', reelSchema);
 
 
 const userSchema = new mongoose.Schema({
-    name : {type: String, default : ""},
-    profileURL : {type: String, default : ""},
-    username : {type: String, default : ""},
-    followers :{type : Number , default : 0},
+    name : { type: String, default : "" },
+    profileURL : { type: String, default : "" },
+    username : { type: String, default : "" },
+    followers : { type : Number , default : 0 },
     user_id: { type: String, required: true },
     access_token: { type: String, required: true },
-    postCount : {type : Number, default : 0},
-    reels: [reelSchema] 
+    postCount : { type : Number, default : 0 },
+    reels: [{ type: mongoose.Schema.Types.ObjectId, ref: "Reel" }] 
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
 
-export default User;
+export { User, Reel };
