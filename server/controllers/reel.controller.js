b6499f9bbfe4d96bc8210ba8bf61e0a7a35615cb
chetannn-github <chetan.rajawat25@gmail.com>
2025-09-19 17:08:03 +0530

    feat - get all posts from ig

--- server/controllers/reel.controller.js
+++ server/controllers/reel.controller.js
@@ -1,6 +1,43 @@
-export const getAllReels = (req, res) => {
-    res.send('Get all reels endpoint');
-};
+import axios from "axios";
+import User from "../models/user.model.js";
+
+export const getAllReels = async(req, res) => {
+    const access_token  = req.user.access_token;
+    const userId = req.user._id;
+    const fields  = ['id','caption','media_url', 'timestamp'].join(',');
+
+    try {
+        const response = await axios.get('https://graph.instagram.com/me/media', {
+            params: {
+                fields : fields,
+                access_token: access_token
+            }
+        });
+
+        let reelsResponse = response.data.data;
+
+        const reelsData = reelsResponse.map((item) => ({
+            reelId: item.id,
+            reelTitle:item.caption || "",
+            mediaURL: item.media_url,
+            isActive: false,
+            keywords: [],
+            message: ""
+        }));
+
+        console.log(userId)
+
+        const updatedUser = await User.findOneAndUpdate(
+            {_id: userId },
+            { $push: { reels: { $each: reelsData } } },
+            { new: true }
+        );
+       
+        return res.json({user : updatedUser})
+    }   catch(error){
+        console.log(error)
+    }
+}
 
 export const addReel = (req, res) => {
     res.send('Add a new reel endpoint');
