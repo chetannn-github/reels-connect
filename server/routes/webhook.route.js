49a0436e6f41b4334bed2b039a067afda63c4e9e
chetannn-github <chetan.rajawat25@gmail.com>
2025-09-19 20:18:24 +0530

    test - webhook

--- server/routes/webhook.route.js
+++ server/routes/webhook.route.js
@@ -11,26 +11,27 @@ router.get("/", verifyWebhook);
 
 // POST for events
 router.post("/", (req, res) => {
-  const signature = req.headers["x-hub-signature-256"];
-
-  if (!signature) {
-    console.log("❌ No signature header found");
-    return res.sendStatus(401);
-  }
+  console.log(req.headers);
+  const signature = req.headers["X-Hub-Signature-256"];
   console.log(APP_SECRET);
+  // if (!signature) {
+  //   console.log("❌ No signature header found");
+  //   return res.sendStatus(401);
+  // }
+ 
 
-  // ✅ Use rawBody here, not JSON.stringify(req.body)
-  const hash = crypto
-    .createHmac("sha256", APP_SECRET)
-    .update(req.rawBody) // 👈 raw body
-    .digest("hex");
+  // // ✅ Use rawBody here, not JSON.stringify(req.body)
+  // const hash = crypto
+  //   .createHmac("sha256", APP_SECRET)
+  //   .update(req.rawBody) // 👈 raw body
+  //   .digest("hex");
 
-  const expectedSignature = `sha256=${hash}`;
+  // const expectedSignature = `sha256=${hash}`;
 
-  if (signature !== expectedSignature) {
-    console.log("❌ Invalid signature");
-    return res.sendStatus(401);
-  }
+  // if (signature !== expectedSignature) {
+  //   console.log("❌ Invalid signature");
+  //   return res.sendStatus(401);
+  // }
 
   console.log("✅ Signature verified");
   console.log("Request body:", JSON.stringify(req.body, null, 2));
