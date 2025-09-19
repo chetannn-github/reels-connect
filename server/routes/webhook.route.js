539b500643a991df8aace109f70a3bc9f9f9e338
chetannn-github <chetan.rajawat25@gmail.com>
2025-09-19 20:14:37 +0530

    fix - webhook

--- server/routes/webhook.route.js
+++ server/routes/webhook.route.js
@@ -1,19 +1,15 @@
 import express from "express";
-import xhub from "express-x-hub";
+import crypto from "crypto";
 import { APP_SECRET } from "../config/env.js";
 import { verifyWebhook } from "../controllers/webhook.controller.js";
-import crypto from "crypto";
 
 const router = express.Router();
-
-router.use(xhub({ algorithm: "sha256", secret: APP_SECRET }));
-
 let received_updates = [];
 
-
+// GET for verification
 router.get("/", verifyWebhook);
 
-
+// POST for events
 router.post("/", (req, res) => {
   const signature = req.headers["x-hub-signature-256"];
 
@@ -21,10 +17,12 @@ router.post("/", (req, res) => {
     console.log("❌ No signature header found");
     return res.sendStatus(401);
   }
+  console.log(APP_SECRET);
 
+  // ✅ Use rawBody here, not JSON.stringify(req.body)
   const hash = crypto
-    .createHmac("sha256", process.env.APP_SECRET)
-    .update(JSON.stringify(req.body)) // 👈 dhyan do ki req.body raw ho
+    .createHmac("sha256", APP_SECRET)
+    .update(req.rawBody) // 👈 raw body
     .digest("hex");
 
   const expectedSignature = `sha256=${hash}`;
@@ -37,6 +35,7 @@ router.post("/", (req, res) => {
   console.log("✅ Signature verified");
   console.log("Request body:", JSON.stringify(req.body, null, 2));
 
+  received_updates.unshift(req.body);
   res.sendStatus(200);
 });
 
