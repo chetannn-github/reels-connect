c7cfcf3e07961754371e079ec6d809612d16697e
chetannn-github <chetan.rajawat25@gmail.com>
2025-09-21 13:43:24 +0530

    fix - typo

--- server/config/env.js
+++ server/config/env.js
@@ -16,4 +16,6 @@ export const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 export const APP_SECRET = process.env.APP_SECRET;
 export const WEBHOOK_VERIFY_TOKEN = process.env.WEBHOOK_VERIFY_TOKEN
 
-export const FRONTEND_BASEURL = "http://localhost:5173/"
+
+export const FRONTEND_BASEURL = "http://localhost:5173" ;
+// export const FRONTEND_BASEURL =  "https://reel-connect.onrender.com";
