5e3b79728ac065df2e5b9ad166203af2c08fa6fb
chetannn-github <chetan.rajawat25@gmail.com>
2025-09-21 10:58:29 +0530

    setup - routes using react router dom

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-import "./App.css"
-function App() {
-  return (
-    <div className=''>App</div>
-  )
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+function Home() {
+  return <h1>Home Page</h1>;
 }
 
-export default App
+function Dashboard() {
+  return <h1>Dashboard Page</h1>;
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
