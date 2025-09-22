b4b64f86e1b443ed0ee216ce83f456b0847014b0
chetannn-github <chetan.rajawat25@gmail.com>
2025-09-22 06:12:03 +0530

    fix - plan selection logic

--- web/src/components/PricingSection.jsx
+++ web/src/components/PricingSection.jsx
@@ -3,9 +3,14 @@ import { Check, Star, Crown, Zap } from "lucide-react";
 import api from "../lib/api"
 import { useNavigate } from "react-router-dom";
 import { RAZORPAY_KEY_ID } from "../lib/constant";
+import { useSelector } from "react-redux";
 
 const PricingSection = () => {
   const navigate = useNavigate();
+  const user = useSelector((state) => state.auth.user);
+  const current_plan = user?.plan;
+
+
   const plans = [
     {
       type : "free",
@@ -194,7 +199,8 @@ const PricingSection = () => {
                     variant={plan.buttonVariant} 
                     size="lg" 
                     className="w-full"
-                    // disabled={plan.disabled}
+                    disabled={plan.type === current_plan || (plan.type === "free" && current_plan !==null && current_plan !== "free")}
+
                   >
                     {plan.buttonText}
                   </Button>
