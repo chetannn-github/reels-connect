e4c9a3b193deab4ad876db256d69dbf0d08aeed1
chetannn-github <chetan.rajawat25@gmail.com>
2025-09-22 06:12:55 +0530

    fix - fetch user when apploads

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,12 +1,34 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import { IGSuccess } from "./pages/IGSuccess";
+import { useDispatch, useSelector } from "react-redux";
+import api from "./lib/api"
+import { setAuth } from "./store/authSlice";
 
 
 function App() {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.auth.user);
+
+  useEffect(() => {
+      const fetchUser = async () => {
+        try {
+          const token = localStorage.getItem("jwt");
+          if(!token) return;
+          
+          const userData = await api.get("/reels", token);
+          dispatch(setAuth({ user: userData, token }));
+         
+        } catch (err) {
+          console.error("Failed to fetch user info:", err);
+        }
+      };
+  
+      fetchUser();
+    }, [dispatch]);
   return (
     <Router>
       <Routes>
