e13f3bd99c7bf31f8898e4bebb87c54e211bb9c9
chetannn-github <chetan.rajawat25@gmail.com>
2025-09-27 13:01:44 +0530

    feat - add generateEmbedding fn

--- server/src/config/openai.js
+++ server/src/config/openai.js
@@ -7,8 +7,14 @@ export async function getOpenAIResponse(input) {
         model: "gpt-5-nano",
         input,
     });
-
     return response.output_text
-
-   
 }
+
+
+export const generateEmbedding = async (text) => {
+  const response = await openAIClient.embeddings.create({
+    model: "text-embedding-3-small",
+    input: text
+  });
+  return response.data[0].embedding;
+};
