d54d0050e422ea1c3e24a4c0060d6c62eae7a9ab
chetannn-github <chetan.rajawat25@gmail.com>
2025-09-28 14:42:48 +0530

    feat - add cors for custom domain

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://reel-connect.onrender.com", "http://localhost:5173"],
+    origin: ["https://reel-connect.onrender.com", "http://localhost:5173", "https://instaconnector.in"],
     credentials: true,
   })
 );
