485db3a57c89c9fcd811e4b96ce42c9d25493281
chetannn-github <chetan.rajawat25@gmail.com>
2025-09-28 19:38:50 +0530

    feat - add cloudinary for file upload

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -29,3 +29,7 @@ export const GROQ_API_KEY = process.env.GROQ_API_KEY;
 export const PINECONE_API_KEY = process.env.PINECONE_API_KEY;
 export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 export const PINECONE_INDEX="reels-connect-vector";
+
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+export const CLOUDINARY_API_SECRET =   process.env.CLOUDINARY_API_SECRET;
--- server/src/controllers/dmAutomation/addDmAutomation.js
+++ server/src/controllers/dmAutomation/addDmAutomation.js
@@ -1,3 +1,4 @@
+import cloudinary from "../../config/cloudinary.js";
 import DMAutomation from "../../models/dmAutomation.model.js";
 
 export const addDmAutomation = async (req, res) => {
@@ -19,16 +20,24 @@ export const addDmAutomation = async (req, res) => {
       return res.status(400).json({ error: "Keyword already exists" });
     }
 
-    
+    let finalCard = card;
+    if (card?.image_url && card.image_url.startsWith("data:image")) {
+      const uploadRes = await cloudinary.uploader.upload(card.image_url, {
+        folder: "dm_automations",
+      });
+      finalCard = {...card,
+        image_url: uploadRes.secure_url,
+      };
+    }
+
     const rule = new DMAutomation({
       user: userId,
       keyword,
       dmMessages: Array.isArray(dmMessages) ? dmMessages : null,
-      card,
-      type : type,
+      card: finalCard,
+      type: type,
       isActive: isActive ?? true,
     });
-
     await rule.save();
 
     return res.json({ message: "DM Automation rule added", rule });
