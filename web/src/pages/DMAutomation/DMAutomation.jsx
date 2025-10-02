100fda4ff69a9b2d44c490e17de42da82b693cd1
chetannn-github <chetan.rajawat25@gmail.com>
2025-10-02 17:50:08 +0530

    refactor - codebase of dmAutomation page

--- web/src/pages/DMAutomation/DMAutomation.jsx
+++ web/src/pages/DMAutomation/DMAutomation.jsx
@@ -1,11 +1,12 @@
 import { useEffect } from 'react';
 
 import { FullScreenLoader } from '../../components/ui/FullScreenLoader';
-import Header from './components/Header';
-import ExistingAutomations from './components/ExistingAutomations';
-import CreateAutomation from './components/CreateAutomation';
+
 import useDMAutomationStore from './hooks/useDMAutomation';
 import { useNavigate } from 'react-router-dom';
+import Header from '../../components/DMAutomation/HeaderSection/Header';
+import CreateAutomation from '../../components/DMAutomation/CreateAutomationSection/CreateAutomation';
+import ExistingAutomations from '../../components/DMAutomation/ExistingAutomationsSection/ExistingAutomations';
 
 
 const DMAutomation = () => {
@@ -17,7 +18,7 @@ const DMAutomation = () => {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-background via-background/50 to-primary/5">
-      <div className="container mx-auto px-4 py-8">
+      <div className="container mx-auto px-0 md:px-4 py-8">
         <Header/>
         <div className="grid lg:grid-cols-2 gap-8 max-w-7xl mx-auto">
           <CreateAutomation/>
